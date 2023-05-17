fbdda312f87caf92fe98294ff15ea2f3fad662d6
Juageta <133536876+Juageta@users.noreply.github.com>
2023-05-17 07:43:01 -0500

    Update Sum_Python.py

--- Sum_Python.py
+++ Sum_Python.py
@@ -1,1 +1,7 @@
 
+idolo = input("¿Quién es tu ídolo? ")
+
+if idolo.lower() == "cristiano ronaldo":
+    print("¡Eso es genial! Cristiano Ronaldo es un increíble futbolista y una inspiración para muchos.")
+else:
+    print("Ese es un gran ídolo también, ¡pero para mí, Cristiano Ronaldo es realmente asombroso!")
